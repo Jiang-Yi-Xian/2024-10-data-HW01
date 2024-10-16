81489689348e3568daa3eaf567af0f5f58423c0a
s1120375@o365st.pu.edu.tw <s1120375@o365st.pu.edu.tw>
2024-10-16 23:00:52 +0800

    加入專案檔案。

---
+++ data-HW01/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+namespace data_HW01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            char[] expression = input.ToCharArray();
+            Stack index = new Stack();
+
+            for (int i = 0; i < expression.Length; i++)
+            {
+                if (expression[i] == '(')
+                {
+                    index.Push(i);
+                }
+                else if (expression[i] == ')')
+                {
+                    if (index.Count > 0)
+                    {
+                        int leftindex = (int)index.Pop();
+                        Console.WriteLine($"{leftindex},{i}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"-1,{i}");
+                    }
+                }
+            }
+            while (index.Count > 0)
+            {
+                int left = (int)index.Pop();
+                Console.WriteLine($"{left},-1");
+            }
+        }
+    }
+}
